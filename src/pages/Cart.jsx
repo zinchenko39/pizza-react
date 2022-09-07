01ab35d22ab01d638a610201e231b45283e75fb6
Vadim <vadim.zinchenco@mail.ru>
2022-09-07 04:05:43 +0200

    Install prettier

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function Cart() {
   return (
     <div>
-        <h1>Корзина</h1>
+      <h1>Корзина</h1>
     </div>
-  )
+  );
 }
 
 export default Cart;
