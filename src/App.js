26e1e92bca4ba7d04b414eda254643902a2fb54b
Vadim <vadim.zinchenco@mail.ru>
2022-09-19 00:42:20 +0200

    Change state to Redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import React from "react";
+import axios from "axios";
+// import store from './redux/store.js';
+import { connect } from "react-redux";
+import { setPizzas as setPizzasAction} from './redux/actions/pizzas.js'
+
 import { Route, Routes } from "react-router-dom";
 
 import { Header } from './components';
 import { Home, Cart } from './pages'
 
-function App() {
+// function App() {
+//   React.useEffect(() => {
+//     axios.get('http://localhost:3000/db.json').then(({data}) => {
+//       setPizzas(data.pizzas);
+//     })
+//   },[]);
 
-  return (
-  <div className="wrapper">
-    <Header/>
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home/>} exact/>
-          <Route path="/cart" element={<Cart/>} exact/>
+//   return (
+//   <div className="wrapper">
+//     <Header/>
+//       <div className="content">
+//         <Routes>
+//           <Route path="/" element={<Home items={pizzas}/>} exact/>
+//           <Route path="/cart" element={<Cart/>} exact/>
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  componentDidMount() {
+    axios.get('http://localhost:3000/db.json').then(({data}) => {
+      this.props.setPizzas(data.pizzas);
+    });
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+     <Header/>
+       <div className="content">
+         <Routes>
+           <Route path="/" element={<Home items={this.props.items}/>} exact/>
+           <Route path="/cart" element={<Cart/>} exact/>
         </Routes>
-      </div>
-    </div>
-  );
+     </div>
+       </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    items: state.pizzas.items,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setPizzas: (items) => dispatch(setPizzasAction(items)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/app.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
 
+
+import store from './redux/store.js';
 import App from './App.js';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+    <Provider store={store}>
+      <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
